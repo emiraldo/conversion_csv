9634d62610f78c70157a6fee25b34cf63294423f
Emiraldo Lozano <emiraldo.lozano@gmail.com>
2020-03-11 18:09:57 -0500

    mensajes de error personalizados, version estable

--- src/invoices/tasks.py
+++ src/invoices/tasks.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 import io
 import csv
+import sys
+
 from celery import shared_task
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
@@ -11,7 +13,11 @@ from invoices.models import UploadedFile, Invoice, InvoiceDetail
 @shared_task()
 def process_csv_file(id, token, csv_file, separator):
     with open(csv_file, 'r') as file:
-        reader = csv.DictReader(file, delimiter=separator, skipinitialspace=True, quoting=csv.QUOTE_ALL)
+        if 't' in separator:
+            reader = csv.DictReader(file, delimiter='\t', skipinitialspace=True)
+
+        else:
+            reader = csv.DictReader(file, delimiter=separator, skipinitialspace=True)
 
         success_row = 0
         error_row = 0
@@ -21,6 +27,27 @@ def process_csv_file(id, token, csv_file, separator):
             row_number_error = 0
             error_description = ''
             dict_row = dict(row)
+
+            if len(list(reader.fieldnames)) != 9:
+
+                channel_layer = get_channel_layer()
+                async_to_sync(channel_layer.group_send)(
+                    token,
+                    {
+                        'type': 'send_message',
+                        'message': {
+                            'success_rows': success_row,
+                            'error_rows': error_row,
+                            'row_number_error': 1,
+                            'error_description': 'Es posible que el separador "'+ separator + '" no sea el correcto para el archivo.',
+                            'id': id
+                        }
+                    }
+                )
+
+                break
+
+
             try:
                 invoice_obj, created = Invoice.objects.get_or_create(
                     number=dict_row['Número de factura'],
@@ -44,7 +71,30 @@ def process_csv_file(id, token, csv_file, separator):
 
 
                 success_row += 1
+            except ValueError as e:
+
+                field_name = str(e).split(': ')[1]
+                error_row += 1
+                row_number_error = current_row
+
+                row_data = list(dict_row.values())
+                row_data_str = ','.join(row_data)
+
+                error_description = 'Error al convertir el campo con valor: ' + field_name + " | " +row_data_str
+
+            except KeyError as e:
+
+                field_name = str(e)
+                error_row += 1
+                row_number_error = current_row
+
+                row_data = list(reader.fieldnames)
+                row_data_str = ', '.join(row_data)
+
+                error_description = 'Error, no se encuentra la columna ' + field_name + "  | " + row_data_str
+
             except Exception as e:
+
                 error_row += 1
                 row_number_error = current_row
                 error_description = str(e)
